8cd6e1f8536cdfc5a4e2728edcceb659b82e9699
mostlystatic <mostlystatic@gmail.com>
2013-01-30 12:03:38 +0000

    First commit.

---
+++ jquery-taphold-class.js
@@ -0,0 +1,29 @@
+// Adds the specified class to the element when it is tapped on continously.
+// This is similar to :active in CSS but for mobile devices.
+// Depends on jQuery Mobile
+// Usage: $(element).tapholdClass("active-element-class");
+$.fn.tapholdClass = function(cls){
+    $(this).each(function(){
+        var element = $(this);
+        var delay = 140;
+        var doAddClass = false;
+        element.on("vmousedown", function(){
+            doAddClass = true;
+            setTimeout(function(){
+                if (doAddClass)
+                {
+                    element.addClass(cls);
+                }
+            }, delay);
+        });
+        element.on("vmouseup", function(){
+                element.removeClass(cls);
+                doAddClass = false;
+        });
+        element.on("taphold", function(){
+            element.removeClass(cls);
+            doAddClass = false;
+        });
+        return this;
+    });
+};
